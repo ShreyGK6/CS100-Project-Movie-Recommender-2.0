3b5108100ad8fbaa1e550c398b0cd86f5653235a
ashah174 <ashah174@ucr.edu>
2023-11-12 14:26:31 -0800

    new changes

---
+++ Instructions.h
@@ -0,0 +1,12 @@
+#ifndef __INSTRUCTIONSPAGE_H__
+#define __INSTRUCTIONSPAGE_H__
+
+class instructions{
+    public:
+        instructions();
+    private:
+        void display();
+        void keyOptions();
+
+};
+#endif
