e4d71062b9bf9ea3f42afce69c44217ede0f133f
ashah174 <ashah174@ucr.edu>
2023-11-13 00:40:28 -0800

    changed code

---
+++ instructionMain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Instructions.h"
+using namespace std;
+
+int main(){
+    instructions instructionsPage;
+    instructionsPage.display();
+    
+    return 0;
+}
+
