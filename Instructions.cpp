22887f76b0eca2c454e1316a0426014880d9d991
ashah174 <ashah174@ucr.edu>
2023-11-13 00:30:09 -0800

    finished page

---
+++ Instructions.cpp
@@ -0,0 +1,31 @@
+#include "Instructions.h"
+//#include "Instructions.cpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+instructions::instructions()
+{}
+
+void instructions::display(){
+    cout << "Our program allows you to see movie recommendations that are personalized to you." << endl;
+    cout << "You can set up your profile and edit it at anytime." << endl; 
+    cout << "Once you reach the menu page, you can type: \n - R to see movie recommendation based on your profile inputs. " << endl;
+    cout << "If insteaad you wish to search, you can type S." << endl;
+    cout << "You will be guided to the search page where you can search by a specific genre, actor, and director." << endl;
+    cout << "You can then sort these by release date and movie rating." << endl;
+    cout << "All of these recommendations will be saved in your account for you to come back at any time" << endl;
+    cout << "Type M to go to menu" << endl;
+    
+
+    // Menu is here. Will be added once merges happenn
+    //cin >> userkey;
+    //if (userkey == M)
+    
+
+
+
+
+
+    
+}
