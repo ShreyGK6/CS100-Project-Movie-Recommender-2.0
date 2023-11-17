f8f644d9b02b0ee04ff719cb56d08f24b17de034
ashah174 <ashah174@ucr.edu>
2023-11-16 23:07:57 -0800

    added code

--- instructiontest.cpp
+++ instructiontest.cpp
@@ -3,5 +3,14 @@
 
 TEST(workingFunction, searchDisplay){
     instructions instructionsPage;
-
-}
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "Hi! Welcome to our movie recommender, where we give you recommendations based on your preferences!")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "You can set up your profile and edit it at anytime.")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "We have features such as:")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "- R: see your movie recommendations")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "- S: searh for movie based on genre, actor, director, etc.")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "Movies can also be sorted out by year!")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "- P: see your settings and change things such as password and username.")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "- Q: Which is to sign out.")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "You can then sort these by release date and movie rating.")
+    EXCPECT_EQ(nstructionsPage.searchDisplay(), "Type M to go to menu and get movie recommendations!")
+} 
