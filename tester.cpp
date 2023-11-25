57277e08d675f21b703f364678fbc3b08ca0f0eb
ayama039 <ayama039@xe-01.cs.ucr.edu>
2023-11-24 21:05:03 -0800

    gitignore and tests

--- tester.cpp
+++ tester.cpp
@@ -5,3 +5,21 @@
 #include <vector>
 #include <string>
 using namespace std; 
+
+TEST (setup2, startup2){
+    userpass setup;
+    prefs options;
+    EXPECT_NO_THROW(setup.startup(options));
+}
+
+TEST (settings, changeprefs){
+    userpass setup;
+    prefs options;
+    Settings profile;
+    EXPECT_NO_THROW(profile.changeprefs(setup, options));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+     return RUN_ALL_TESTS();
+  }
